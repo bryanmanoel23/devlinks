40e1ef25a1a4a4a0c1b305ee3ca678175f606058
bryanmanoel23 <110640854+bryanmanoel23@users.noreply.github.com>
2023-09-20 14:37:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function test() {
+    const test = document.getElementById('test')
+    if(test.classList.contains('light')){
+        test.classList.remove('light')
+        const img = document.getElementById('img')
+        img.src = "./assets/assets/fotor_2023-9-16_10_46_28-fotor-20230916104721.png"
+    }else{
+    test.classList.add("light")
+    const img = document.getElementById('img')
+    img.src = "./assets/assets/fotor_2023-9-19_15_56_44-fotor-20230919155726.png"
+    }
+}
+
+function inacabado() {
+    alert('Este site está em desenvolimento, novidades em breve !')
+}
